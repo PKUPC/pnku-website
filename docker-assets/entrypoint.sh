0ddb98d3ea8510cdfac7bcff4cd99a26412a674d
kinami <36267804+kinami0331@users.noreply.github.com>
2024-11-04 01:23:53 +0800

    [Misc] Update docker config.

--- docker-assets/entrypoint.sh
+++ docker-assets/entrypoint.sh
@@ -6,7 +6,7 @@ mkdir -p /app/log/backend
 python3 /app/backend/scripts/gen_nginx_config.py > /etc/nginx/sites-enabled/website.conf
 python3 /app/backend/scripts/gen_nginx_config.py > /app/log/nginx/website.conf
 
-python3 /app/backend/run_reducer_admin.py >> /app/log/backend/reducer.log 2>&1 &
-python3 /app/backend/run_worker_api.py >> /app/log/backend/worker.log 2>&1 &
+python3 -u /app/backend/run_reducer_admin.py >> /app/log/backend/reducer.log 2>&1 &
+python3 -u /app/backend/run_worker_api.py >> /app/log/backend/worker.log 2>&1 &
 
 nginx -g 'daemon off;'
