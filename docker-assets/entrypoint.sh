838f8466f2866b0501a1fa750a34b17e8c659991
kinami <36267804+kinami0331@users.noreply.github.com>
2025-09-24 06:12:03 +0800

    [Misc] Update docker config.

--- docker-assets/entrypoint.sh
+++ docker-assets/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+service cron start
+echo "0 4 * * * /usr/sbin/logrotate /etc/logrotate.d/nginx" | crontab -
+
 mkdir -p /app/log/nginx
 mkdir -p /app/log/backend
 
 python3 /app/backend/scripts/gen_nginx_config.py > /etc/nginx/sites-enabled/website.conf
 python3 /app/backend/scripts/gen_nginx_config.py > /app/log/nginx/website.conf
 
-python3 -u /app/backend/run_reducer_admin.py >> /app/log/backend/reducer.log 2>&1 &
-python3 -u /app/backend/run_worker_api.py >> /app/log/backend/worker.log 2>&1 &
+TIMESTAMP=$(date +%Y%m%d_%H%M%S)
+python3 -u /app/backend/run_all.py >> /app/log/backend/${TIMESTAMP}.log 2>&1 &
 
 nginx -g 'daemon off;'
