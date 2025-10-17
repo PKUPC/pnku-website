492a7fab6a71f4c9cc753d4a05075e61e01c25f6
kinami <36267804+kinami0331@users.noreply.github.com>
2025-10-17 18:48:58 +0800

    [Frontend] Rename attention to hint_point.

--- frontend/src/types/wish.ts
+++ frontend/src/types/wish.ts
@@ -3,7 +3,7 @@ import React from 'react';
 // ADHOC: 在这里修改各种 adhoc 类型定义
 export namespace Adhoc {
     // api 交互时统一为小写，大写字母形式显示在 url 中很怪
-    export type CurrencyType = 'attention';
+    export type CurrencyType = 'hint_point';
 }
 
 export namespace Wish {
