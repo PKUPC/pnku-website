814918862fe1ca486cbe5cd6ae3b2e907d09d415
starxingchenc <1340364178@qq.com>
2026-03-16 00:33:14 +0800

    [Backend] Move some functions to src.api.utils.

--- backend/src/api/utils.py
+++ backend/src/api/utils.py
@@ -1,8 +1,13 @@
 from __future__ import annotations
 
-from sanic.request import Request
+import httpx
 
-from src import utils
+from sanic import HTTPResponse, response
+from sanic.exceptions import SanicException
+from sanic.request import Request
+from sanic_ext.exceptions import ValidationError
+
+from src import secret, utils
 from src.state import User
 from src.store.user_store import EmailLoginPropertyModel
 
@@ -39,3 +44,37 @@ def get_cur_user(req: Request) -> User | None:
                     return None
 
     return user
+
+
+def get_http_client(_req: Request) -> httpx.AsyncClient:
+    return httpx.AsyncClient(
+        http2=True,
+        mounts=secret.OAUTH_HTTP_MOUNTS,
+        timeout=secret.OAUTH_HTTP_TIMEOUT,
+    )
+
+
+async def handle_error(req: Request, exc: Exception) -> HTTPResponse:
+    if isinstance(exc, SanicException):
+        if isinstance(exc, ValidationError):
+            return response.json({'status': 'error', 'title': 'INVALID PARAMS', 'message': '参数格式错误'})
+        raise exc
+
+    try:
+        user = get_cur_user(req)
+        debug_info = f'{req.id} {req.uri_template} U#{"--" if user is None else user.model.id}'
+    except Exception as e:
+        debug_info = f'no debug info, {repr(e)}'
+
+    req.app.ctx.worker.log(
+        'error', 'app.handle_error', f'exception in request ({debug_info}): {utils.get_traceback(exc)}'
+    )
+    return response.html(
+        '<!doctype html>'
+        '<h1>🤡 500 — Internal Server Error</h1>'
+        '<p>This accident is recorded.</p>'
+        f'<p>If you believe there is a bug, tell admin about this request ID: {req.id}</p>'
+        '<br>'
+        '<p>😭 <i>Project Guiding Star</i></p>',
+        status=500,
+    )
